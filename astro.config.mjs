8fdb6c7937f51d0baf848bd309da100ff6276a78
Aleksandr Elkin <55530374+aleks-elkin@users.noreply.github.com>
2024-12-06 16:45:31 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
    site: 'https://aleks-elkin.github.io',
-   base: 'astro-blog',
+   base: '/astro-blog',
 });
